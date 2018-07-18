ded5f36a644934234497b5b847310c8a7ab29c00
byeongsoo <byeongsoo@ggt593>
2018-07-18 12:24:59 +0900

    컨트롤러 수정

--- finalproj/src/main/java/com/jhta/fproj/JunController.java
+++ finalproj/src/main/java/com/jhta/fproj/JunController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.jhta.fproj.model.Jun_DAO;
 import com.jhta.fproj.model.Jun_VO;
-import com.jhta.fproj.model.MngDAO;
+import com.jhta.fproj.model.PageVO;
 
 @Controller
 
@@ -20,26 +20,20 @@ public class JunController {
 
 	@Resource
 	Jun_DAO dao;
-	
-	@Resource
-	MngDAO mngDAO;
 
 	@RequestMapping("jun_List/{main}") 
 	String view(@PathVariable("main") String main,Model model) {
-		System.out.println("jun_List/{main} 탔음@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
-		System.out.println("컨트롤러 alert 가는 if문 타기전");
-		System.out.println(main+"   메인은 어디인가!@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
-
 		
 		if (main.equals("course_insertReg")||main.equals("course_ModifyReg")||main.equals("course_Delete")) {
-			System.out.println("컨트롤러 alert 가는 if문 탐");
+			
 			return "jun_List/alert";
 		}
+		
 		switch (main) {
 		
 		
 		case "course_List":
-			System.out.println("리퀘스트맵핑 course_List 들어옴");
+			
 
 			model.addAttribute("service", "jun_List");
 			model.addAttribute("main",  "course_List");
@@ -68,7 +62,6 @@ public class JunController {
 			
 		case"p_Course_List":
 			
-			System.out.println("리퀘스트맵핑 p_Course_List 들어옴");
 			model.addAttribute("service", "jun_List");
 			model.addAttribute("main",  "p_Course_List");
 			model.addAttribute("list", "p_Course_List4.jsp");
@@ -76,21 +69,21 @@ public class JunController {
 			break;
 			
 		case"p_Course_List1":
-			System.out.println("리퀘스트맵핑 p_Course_List1 들어옴");
+			
 			model.addAttribute("service", "jun_List");
 			model.addAttribute("main",  "p_Course_List");
 			model.addAttribute("list", "p_Course_List1.jsp");
 			break;
 			
 		case"p_Course_List2":
-			System.out.println("리퀘스트맵핑 p_Course_List2 들어옴");
+			
 			model.addAttribute("service", "jun_List");
 			model.addAttribute("main",  "p_Course_List");
 			model.addAttribute("list", "p_Course_List2.jsp");
 			break;
 			
 		case"p_Course_List3":
-			System.out.println("리퀘스트맵핑 p_Course_List3 들어옴");
+		
 			model.addAttribute("service", "jun_List");
 			model.addAttribute("main",  "p_Course_List");
 			model.addAttribute("list", "p_Course_List3.jsp");
@@ -104,53 +97,57 @@ public class JunController {
 	}
 	
 	@ModelAttribute("data")
-	Object data(@PathVariable("main") String main,Jun_VO vo, Model model,HttpSession session) {
-
+	Object data(@PathVariable("main") String main,Jun_VO vo,Model model,HttpSession session) {
+		
+		System.out.println(" jun 컨트롤러 진입 :" + vo);
+		
 		model.addAttribute("service", "jun_List");
-		System.out.println(" 컨트롤러에서 어트리뷰트 data값을 받음 :" + vo);
-
+		vo.setCid((String)session.getAttribute("id"));
+		
+		
 		Object res = null;
 
 		switch (main) {
 
 		case "course_register":
+			System.out.println("과목등록  case: course_register:" + vo);
 			res = dao.professor_list(vo);
-			System.out.println("컨트롤러에서  case: course_register 들어옴" + vo);
 			break;
 		
-		/*		관리자가 보는 목록보@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*/
+		/*		관리자@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*/
 		case "course_List":
+			System.out.println("강의 전체:" + vo);
 			res = dao.course_list(vo);
-			System.out.println("컨트롤러에서  case: course_List 들어옴" + vo);
-	
+
 			break;
 
 		case "course_List1":
+			System.out.println("개강예정:" + vo);
 			model.addAttribute("list", "List1");
 			res = dao.course_list1(vo);
-			System.out.println("컨트롤러에서  case: course_List1 들어옴" + vo);
+
 			break;
 			
 		case "course_List2":
-			System.out.println("컨트롤러에서  case: course_List2 들어옴" + vo);
+			System.out.println("진행중:" + vo);
 			res = dao.course_list2(vo);
 
 			break;
 			
 		case "course_List3":
+			System.out.println("종료:" + vo);
 			res = dao.course_list3(vo);
-			System.out.println("컨트롤러에서  case: course_List3 들어옴" + vo);
 
 			break;
 			
 	/*	case "course_List4":
-			System.out.println("컨트롤러에서  case: course_List4 들어옴" + vo);
+			System.out.println("而⑦듃濡ㅻ윭�뿉�꽌  case: course_List4 �뱾�뼱�샂" + vo);
 			res = dao.course_list4(vo);
 
 			break;*/
 			
 		case "course_insertReg":
-			System.out.println("컨트롤러에서  case: insertReg 들어옴" + vo);
+			System.out.println("관리자  case: insertReg:" + vo);
 			System.out.println("@@@@@@@@@");
 			System.out.println("@@@@@@@@@@@");
 			System.out.println("@@@@@@@@@@@@@");
@@ -158,16 +155,16 @@ public class JunController {
 			
 			res = dao.course_insert(vo);
 
-			model.addAttribute("msg", "작성완료");
+			model.addAttribute("msg", "등록성공");
 			model.addAttribute("url", "course_Detail?ccode="+vo.getCcode());
 
 			break;
 			
 			
 		case "course_Detail":	
-
+			System.out.println("과목 정보:" +vo);
 			res = dao.course_detail(vo);
-			System.out.println("컨트롤러에서  case: course_Detail 들어옴" +vo);
+			
 			System.out.println(vo.getCstart());
 			System.out.println(vo.getCfinish());
 			
@@ -175,28 +172,25 @@ public class JunController {
 			
 			
 		case "course_Modify":	
-		
+			System.out.println("관리자  case: course_Modify:" + vo);
 			res = dao.course_modify(vo);
 			
-			model.addAttribute("teacher", dao.course_modify1(vo));
-			
-			System.out.println("컨트롤러에서  case: course_Modify 들어옴" + vo);
 			break;
 		
 		case "course_ModifyReg":
-
+			System.out.println("과목 수정:" +vo);
 			res = dao.course_modifyReg(vo);
-			System.out.println("컨트롤러에서  case: course_ModifyReg 들어옴" +vo);
-			model.addAttribute("msg", "수정완료");
+
+			model.addAttribute("msg", "수정성공");
 			model.addAttribute("url", "course_Detail?ccode=" + vo.getCcode());
 
 			break;
 		
 		case "course_Delete":
-			System.out.println("컨트롤러에서  case: course_Delete 들어옴" + vo);
+			System.out.println("과목삭제:" + vo);
 			res = dao.course_delete(vo);
 	
-			model.addAttribute("msg", "삭제 완료");
+			model.addAttribute("msg", "삭제성공");
 			model.addAttribute("url", "course_List");
 
 			break;
@@ -204,14 +198,14 @@ public class JunController {
 			
 			
 		case "astudent_List":
-			System.out.println("컨트롤러에서  case: astudent_List 들어옴" + vo);
+			System.out.println("학생 목록:" + vo);
 			res = dao.astudent_list(vo);
 	
 			break;
 			
 			
 		case "astudent_Course_List":
-			System.out.println("컨트롤러에서  case: astudent_Course_List 들어옴" + vo);
+			System.out.println("학생이 수강신청한 과목 목록:" + vo);
 			res = dao.astudent_course_list(vo);
 	
 			break;
@@ -219,52 +213,50 @@ public class JunController {
 			
 			
 		case "professor_List":
-			System.out.println("컨트롤러에서  case: professor_List 들어옴" + vo);
+			System.out.println("강사목록:" + vo);
 			res = dao.professor_list(vo);
 	
 			break;
 			
 		case "professor_Course_List":
-			System.out.println("컨트롤러에서  case: professor_Course_List 들어옴" + vo);
+			System.out.println("강사의 강의목록:" + vo);
 			res = dao.professor_course_list(vo);
 	
 			break;
 
 			
-			/*		강사가 보는 목록@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*/
+			/*		강사@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*/
 			
 		case "p_Course_List":
-			vo.setCid((String)session.getAttribute("id"));
-			System.out.println("컨트롤러에서  case: p_Course_List 들어옴" + vo);
+			System.out.println("과목전체:" + vo);
 			res = dao.p_course_list(vo);
 	
 			break;
 			
 		case "p_Course_List1":
-			vo.setCid((String)session.getAttribute("id"));
-			System.out.println("컨트롤러에서  case: p_Course_List1 들어옴" + vo);
+			System.out.println("진행중인과목:" + vo);
 			res = dao.p_course_list1(vo);
 	
 			break;
 			
 			
 		case "p_Course_List2":
-			vo.setCid((String)session.getAttribute("id"));
-			System.out.println("컨트롤러에서  case: p_Course_List2 들어옴" + vo);
+			System.out.println("종료된과목:" + vo);
 			res = dao.p_course_list2(vo);
 	
 			break;
 			
 		case "p_Course_List3":
-			vo.setCid((String)session.getAttribute("id"));
-			System.out.println("컨트롤러에서  case: p_Course_List3 들어옴" + vo);
+			System.out.println("개설예정과목:" + vo);
 			res = dao.p_course_list3(vo);
 	
 			break;
 			
 			
+			
+			
 		case "p_Astudent_List":
-			System.out.println("컨트롤러에서  case: p_Course_List 들어옴" + vo);
+			System.out.println("강사  case: p_Astudent_List:" + vo);
 			res = dao.p_astudent_list(vo);
 	
 			break;
